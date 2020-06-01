8653d52e64d199cf83a5f872f3a47d5e3d07a0b7
WylderMSM <wyldermenezes@gmail.com>
2020-06-01 17:51:04 -0300

    first commit

---
+++ src/main/java/com/wylderm/workshopmongo/WorkshopmongApplication.java
@@ -0,0 +1,13 @@
+package com.wylderm.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopmongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopmongApplication.class, args);
+	}
+
+}
